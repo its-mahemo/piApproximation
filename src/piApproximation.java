bc9c431ce8cea386f06d8a1e5227b78c7bda80df
matthias-42 <48037688+m21m@users.noreply.github.com>
2020-11-01 09:55:39 +0100

    minor fixes

--- src/piApproximation.java
+++ src/piApproximation.java
@@ -4,9 +4,9 @@ public class piApproximation {
     public static void main(String[] args) {
         /* This program approximates the value of pi using a statistical approach based on a
         Monte-Carlo-Algorithm with one million iterations. It counts the number of times a random
-        point lies inside a quarter circle with radius 1 which is on average proportional to the
-        quarter circle's area. We are then able to derive the value of pi as four times that value
-        as the area of the whole circle equals pi (area = pi * r^2 = pi * 1^2 = pi) */
+        point lies inside a quarter circle with radius 1, which is on average proportional to the
+        quarter circle's area. We are then able to derive the value of pi as four times that value,
+        because the area of the whole circle equals pi (area = pi * r^2 = pi * 1^2 = pi) */
 
         double numIterations = 1000000; // Number of iterations to be calculated
         System.out.println("The value of pi is approximately: " + approximatePi(numIterations));
